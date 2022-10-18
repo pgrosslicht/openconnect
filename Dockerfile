f8108148ab6ef7b02895258aa170f4b1b5531703
Patrick Grosslicht <patrick@grosslicht.com>
2022-10-18 14:21:21 +0200

    Use vpn-splice to allow for split-tunneling.

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,28 @@
-FROM docker.io/alpine:3.13
-LABEL maintainer='Alex Wicks <alex@awicks.io>'
+FROM alpine:3.16
 
 # BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
 # COMMIT_SHA="$(git rev-parse HEAD 2>/dev/null || echo 'null')"
-ARG BUILD_DATE COMMIT_SHA
+ARG BUILD_DATE
+ARG COMMIT_SHA
 
 # https://github.com/opencontainers/image-spec/blob/master/spec.md
 LABEL org.opencontainers.image.title='openconnect' \
       org.opencontainers.image.created="${BUILD_DATE}" \
       org.opencontainers.image.description='AnyConnect-compatible client to route host traffic' \
-      org.opencontainers.image.documentation='https://github.com/aw1cks/openconnect/blob/master/README.md' \
+      org.opencontainers.image.documentation='https://github.com/pgrosslicht/openconnect/blob/master/README.md' \
       org.opencontainers.image.version='1.0' \
-      org.opencontainers.image.source='https://github.com/aw1cks/openconnect' \
+      org.opencontainers.image.source='https://github.com/pgrosslicht/openconnect' \
       org.opencontainers.image.revision="${COMMIT_SHA}"
 
-RUN apk add --no-cache openconnect dnsmasq
+RUN apk add --no-cache openconnect \
+    # add vpn-slice with dependencies (dig) https://github.com/dlenski/vpn-slice
+    && apk add --no-cache python3 bind-tools py3-pip \
+    && pip3 install "vpn-slice[dnspython,setproctitle]"
 
+COPY ./entrypoint.sh /vpn/entrypoint.sh
 WORKDIR /vpn
-COPY ./entrypoint.sh .
 
 HEALTHCHECK --start-period=15s --retries=1 \
-  CMD pgrep openconnect || exit 1; pgrep dnsmasq || exit 1
+  CMD pgrep openconnect || exit 1
 
 ENTRYPOINT ["/vpn/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,6 @@
 # github.com/aw1cks
 #
 
-get_resolv_nameservers() {
-  grep '^nameserver' /etc/resolv.conf | awk '{print $2}'
-}
-
 printf "\e[32m
     ___      ___      ___       __      ___      ___       __       __      ___      ___    __  ___ 
   //   ) ) //   ) ) //___) ) //   ) ) //   ) ) //   ) ) //   ) ) //   ) ) //___) ) //   ) )  / /    
@@ -37,9 +33,16 @@ then
 fi
 printf "\e[33mPassword:\e[0m [REDACTED]\n\n"
 
+if [ -z "${HOSTNAMES}" ]
+then
+  printf "\e[31m\$HOSTNAMES is not set\e[0m\n"
+  exit 2
+fi
+printf "\e[33mHostnames:\e[0m %s\n" "${HOSTNAMES}"
+
 printf "\e[32mSetting mandatory arguments...\e[0m\n"
 # Set user
-OPENCONNECT_ARGS="--background --user=${USER} -i tun127 --passwd-on-stdin --non-inter"
+OPENCONNECT_ARGS="--user=${USER} -i tun127 --passwd-on-stdin --non-inter"
 
 # Test for auth group
 printf "\e[32mChecking for authentication group parameter...\e[0m\n"
@@ -60,60 +63,7 @@ printf "\e[32mSetting URL...\e[0m\n"
 OPENCONNECT_ARGS="${OPENCONNECT_ARGS} ${URL}"
 
 printf "\e[32mStarting OpenConnect VPN...\e[0m\n"
-printf "\e[33mArguments:\e[0m %s\n\n" "${OPENCONNECT_ARGS}"
+OPENCONNECT_CMD="openconnect --script='vpn-slice ${HOSTNAMES}' ${OPENCONNECT_ARGS}"
+printf "\e[33mArguments:\e[0m %s\n\n" "${OPENCONNECT_CMD}"
 # shellcheck disable=SC2086
-(echo "${PASS}"; [ -n "${OTP}" ] && echo "${OTP}") | openconnect ${OPENCONNECT_ARGS}
-
-# Add our initial dnsmasq config
-printf '# Static options
-no-resolv
-strict-order
-
-# Dynamically populated upstream servers\n' > /etc/dnsmasq.d/dns.conf
-
-# Get the pre-VPN state from resolv.conf
-OLD_RESOLV=$(get_resolv_nameservers)
-
-# Sleep until the VPN comes up
-# Otherwise the VPNC script won't populate resolv.conf
-while ! grep -q VPNC /etc/resolv.conf
-do
-  sleep 5
-done
-
-# Add our dNAT now - since we know the VPN is up due to above
-iptables -t nat -A POSTROUTING -o tun127 -j MASQUERADE
-
-# Get our DNS Servers again, now that the VPNC script updated them
-# Then populate the servers in dnsmasq config
-NEW_RESOLV=$(get_resolv_nameservers)
-
-for LOCAL_DOMAIN_SRV in ${OLD_RESOLV}
-do
-  printf "server=%s\n" "${LOCAL_DOMAIN_SRV}" >> /etc/dnsmasq.d/dns.conf
-done
-
-# Add any extra search domains, if specified
-# Append them, after existing search domains, so that the host resolv.conf takes precedence
-if [ -n "${SEARCH_DOMAINS}" ]
-then
-  # Docker bind mounts resolv.conf at runtime
-  # This weirdness bypasses an error that looks as such:
-  # sed: can't move '/etc/resolv.confahaCgd' to '/etc/resolv.conf': Resource busy
-  # shellcheck disable=SC2005
-  echo "$(sed "/search/ s/$/ ${SEARCH_DOMAINS}/" /etc/resolv.conf)" > /etc/resolv.conf
-  # Add search domains as forwarders.
-  # This prevents DNS leaks, since only these domains will be resolved via VPN-configured DNS servers.
-  printf '
-  # DNS forwarders\n' >> /etc/dnsmasq.d/dns.conf
-  for REMOTE_DOMAIN_SRV in ${NEW_RESOLV}
-  do
-    for DOMAIN in ${SEARCH_DOMAINS}
-    do
-      printf "server=/%s/%s\n" "${DOMAIN}" "${REMOTE_DOMAIN_SRV}" >> /etc/dnsmasq.d/dns.conf
-    done
-  done
-fi
-
-printf "\n\e[32mStarting dnsmasq...\e[0m\n"
-dnsmasq -k --log-facility=- -C /etc/dnsmasq.d/dns.conf
+(echo "${PASS}"; [ -n "${OTP}" ] && echo "${OTP}") | eval ${OPENCONNECT_CMD}
